6623c17599757dddeb79bd30e997759fe0d7ec4a
Douglas Lisboa <douglas.lisboa@edu.unipar.br>
2020-06-16 01:27:28 -0300

    Concluido desafio NodeJS -Implementado crud com os methos POST, GET, DELETE, PUT. -Utilizado query params.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid,isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,81 @@ app.use(cors());
 
 const repositories = [];
 
+
+function validateProjectParams(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Tudo invalidado'})
+  }
+  return next();
+}
+
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { id, title, url, techs, likes } = request.body
+
+  const repositorio = { id: uuid(), title, url, techs, likes:0};
+
+  repositories.push(repositorio);
+
+  return response.json(repositorio);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title,  url, techs} = request.body;
+
+  const repositoriesFindId = repositories.findIndex(repositorio => repositorio.id == id);
+
+  if( repositoriesFindId < 0) {
+    return response.status(400).send();
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoriesFindId].likes
+  };
+
+  repositories[repositoriesFindId] = repositorie;
+
+  return response.json(repositorie)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoriesFindId =  repositories.findIndex(repositorio => repositorio.id == id);
+
+  if(repositoriesFindId < 0) {
+    return response.status(400).json({ error: 'Not found id in repositories.'})
+  }
+
+  repositories.splice(repositoriesFindId, 1);
+  return response.send(204).send();
 });
 
+
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repositorio => repositorio.id == id);
+
+  if(!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository)
+
 });
 
 module.exports = app;
